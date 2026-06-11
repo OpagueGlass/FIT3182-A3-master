1e3a19806ef07bf3d41443d553623b7f3fea7e1b
Jona0002 <jona0002@student.monash.edu>
2026-06-12 02:42:23 +0800

    fix start script removing checkpoints

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ sudo docker compose up -d
 
 sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/start-master.sh'
 
-sudo rm -rf /mnt/shared
+sudo rm -rf /mnt/shared/checkpoints/*
