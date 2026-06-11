21a3f17e0ae2d89a46f85d98de88935d01865fac
Jona0002 <jona0002@student.monash.edu>
2026-06-11 21:24:12 +0800

    fixes stop script

--- start_spark.sh
+++ start_spark.sh
@@ -1,5 +1,3 @@
 sudo docker exec -it fit3182-a3-master-pyspark-1 python /home/student/src/setup_mongodb.py
 
 sudo docker exec -it fit3182-a3-master-pyspark-1 python /home/student/src/streaming_app.py
-
-sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/stop-master.sh'
---
+++ stop.sh
@@ -0,0 +1,3 @@
+sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/stop-master.sh'
+
+sudo docker compose stop
