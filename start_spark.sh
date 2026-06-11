5ef520063537ca530513246a42ad19a510af0ab9
Jona0002 <jona0002@student.monash.edu>
2026-06-11 18:12:01 +0800

    updates start spark script

--- start_spark.sh
+++ start_spark.sh
@@ -1,4 +1,4 @@
-sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/start-master.sh $SPARK_MASTER_URL'
+sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/start-master.sh'
 
 sudo docker exec -it fit3182-a3-master-pyspark-1 python /home/student/src/setup_mongodb.py
 
