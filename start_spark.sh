6fc4460028845150f8caa5d52abc02257884a025
Jona0002 <jona0002@student.monash.edu>
2026-06-11 17:46:16 +0800

    Updates compose and start spark script to start master

--- start_spark.sh
+++ start_spark.sh
@@ -1,3 +1,5 @@
+sudo docker exec -it fit3182-a3-master-pyspark-1 bash -c '$SPARK_HOME/sbin/start-master.sh $SPARK_MASTER_URL'
+
 sudo docker exec -it fit3182-a3-master-pyspark-1 python /home/student/src/setup_mongodb.py
 
 sudo docker exec -it fit3182-a3-master-pyspark-1 python /home/student/src/streaming_app.py
